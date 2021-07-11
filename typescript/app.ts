6ad836b0daadfcd7ff051d80bf7dd401ace9db3b
kanozad <doug.kanoza@gmail.com>
2021-07-11 15:01:27 -0400

    updates

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,9 +1,10 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+const person: {
+    name: string;
+    age: number;
+    nickname?: string;
+} = {
+    name: 'Binky',
+    age: 243
 }
 
-const number1 = 5;
-const number2 = 2.8;
-
-const result = add(number1, number2);
-console.log(result);
+console.log(person);
